e8435cddff13585e13a59050f57089fa9405ecfb
Shadowstep <144902634+mixalis-tara@users.noreply.github.com>
2024-05-04 21:06:33 +0300

    i made new methods. and make inputs and add for each input a method.

--- main.py
+++ main.py
@@ -2,4 +2,39 @@ from services.services import *
 from students.students import *
 
 create_students()
-display_students()
+
+highest_student = highest_level_students(objs, level)
+smallest_student = smallest_level_students(objs, level)
+display_all_students = display_students(objs)
+
+while True:
+    input_field = input("Welcome to our students application!\nGive a number to start\nPress 1! Display All\n"
+                        "Press 2! Mesos oros Hlikias\nPress 3! low lvl mathites"
+                        "\nPress 4! high lvl mathites \nPress 5! exit : ")
+    if input_field == "1":
+        if display_all_students:
+            for student in display_all_students:
+                print(f"{student}\n")
+    elif input_field == "2":
+        mesos_oros_hlikias = print(f"O mesos oros hlikias twn mathitwn einai : {average_age(age)}\n")
+    elif input_field == "3":
+        if smallest_student:
+            print(f"Oi mathites me to ligotero lvl({smallest_lvl(level)}) einai:")
+            for student in smallest_student:
+                print(f"{student}\n")
+    elif input_field == "4":
+        if highest_student:
+            print(f"Oi mathites me to megalitero lvl({highest_lvl(level)}) einai:")
+            for student in highest_student:
+                print(f"{student}\n")
+    elif input_field == "5":
+        break
+
+
+
+
+
+
+
+
+
--- services/services.py
+++ services/services.py
@@ -1,9 +1,9 @@
 from students.students import Students
 
-f_name = ["Giannis", "Maria", "Kwstas"]
-l_name = ["Papadopoulos", "Karagianni", "Dhmitrioy"]
-age = [15, 16, 14]
-level = [2, 3, 1]
+f_name = ["Giannis", "Maria", "Kwstas", "Eleni", "Nikos", "Antwnhs", "Aggelikh", "Dimitris"]
+l_name = ["Papadopoulos", "Karagianni", "Dhmitrioy", "Papanikolaou", "Andreou", "Mixahlidis", "Papadaki", "Kontogiannis"]
+age = [15, 16, 14, 15, 17, 15, 14, 16]
+level = [2, 3, 1, 2, 3, 2, 1, 3]
 
 objs = list()
 
@@ -12,7 +12,35 @@ def create_students():
     for f, l, a, le in zip(f_name, l_name, age, level):
         student = Students(f, l, a, le)
         objs.append(student)
-def display_students():
-    for obj in objs:
-        print(f"Onomateponimo: {obj.f_name} {obj.l_name}\nHlikia: {obj.age}\nEpipedo: {obj.level}\n")
+def display_students(objs):
+    display_all_students = [(f"Onomateponimo: {obj.f_name} {obj.l_name}\nHlikia: {obj.age}\nEpipedo: {obj.level}\n")for obj in objs]
+    return display_all_students
+
+def average_age(age):
+    return sum(age) / len(age)
+
+def highest_level_students(objs, level):
+    highest = max(level)
+    highest_students = [(f"Mathitis: {obj.f_name} {obj.l_name}") for obj in objs if obj.level == highest]
+    return highest_students
+
+def highest_lvl(level):
+    highest = max(level)
+    return highest
+
+def smallest_level_students(objs, level):
+    smallest = min(level)
+    smallest_students = [(f"Mathitis: {obj.f_name} {obj.l_name}") for obj in objs if obj.level == smallest]
+    return smallest_students
+
+def smallest_lvl(level):
+    smallest = min(level)
+    return smallest
+
+
+
+
+
+
+
 
